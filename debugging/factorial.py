ef700aca4a9e332335e626912ec31207ca5bd338
ispdls <is.pdls90@gmail.com>
2024-09-03 21:59:27 -0400

    chat gpt

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python factorial.py <number>")
-    sys.exit(1)
-
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # Decrement n in each iteration
     return result
 
-try:
-    f = factorial(int(sys.argv[1]))
-    print(f)
-except ValueError:
-    print("Error: Please provide a valid integer.")
-    sys.exit(1)
+f = factorial(int(sys.argv[1]))
+print(f)
