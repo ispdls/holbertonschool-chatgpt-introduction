b069bcd63dea9caffe7c6f96bcd5510b7129d6dd
ispdls <is.pdls90@gmail.com>
2024-09-03 21:25:38 -0400

    chat gpt

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,8 +5,9 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrease n in each iteration to reach the base case (n == 1)
+        n -= 1
     return result
+
 
 f = factorial(int(sys.argv[1]))
 print(f)
